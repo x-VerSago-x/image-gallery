4942d295c7e3df5cd5cb9d47bc3d9e974ba8a853
x-VerSago-x <91416109+x-VerSago-x@users.noreply.github.com>
2021-11-08 16:51:43 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-var displayedImage = document.querySelector('.displayed-img')
-var thumbBar = document.querySelector('.thumb-bar')
+const displayedImage = document.querySelector('.displayed-img')
+const thumbBar = document.querySelector('.thumb-bar')
 const dark = document.querySelector('.dark')
 let transparent = 0
 btn = document.querySelector('button')
-var overlay = document.querySelector('.overlay')
+const overlay = document.querySelector('.overlay')
+const nextPrev = document.querySelectorAll('p')
 let select
 // можно использовать
 const pathImages = [
@@ -13,14 +14,7 @@ const pathImages = [
   'images/pic4.jpg',
   'images/pic5.jpg',
 ]
-
-for (let i = 0; i <= pathImages.length - 1; i++) {
-  var newImage = document.createElement('img')
-  newImage.setAttribute('src', pathImages[i])
-  thumbBar.appendChild(newImage)
-}
-
-thumbBar.addEventListener('click', (e) => {
+function setSelect(e) {
   select = document.querySelector('.select')
   if (select) {
     select.classList.remove('select')
@@ -28,10 +22,25 @@ thumbBar.addEventListener('click', (e) => {
   } else {
     e.target.classList.add('select')
   }
-
   displayedImage.setAttribute('src', e.target.getAttribute('src'))
-})
+}
+
+for (let i = 0; i <= nextPrev.length - 1; i++) {
+  nextPrev[i].parentElement.addEventListener('click', (e) => {
+    console.log('test right and left panel' + e)
+  })
+}
+
+for (let i = 0; i <= pathImages.length - 1; i++) {
+  var newImage = document.createElement('img')
+  newImage.setAttribute('src', pathImages[i])
+  thumbBar.appendChild(newImage)
+}
+
+thumbBar.addEventListener('click', setSelect)
+
 dark.addEventListener('click', () => {
+  console.log('test click')
   transparent++
   if (transparent === 10) {
     overlay.style.backgroundColor = 'rgba(0,0,0,1)'
