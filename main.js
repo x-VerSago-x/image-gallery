99ac51cfd49f64dac18ba1597097f5f5358fd7a8
x-VerSago-x <91416109+x-VerSago-x@users.noreply.github.com>
2021-11-11 16:55:48 +0900

    Update main.js

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ const dark = document.querySelector('.dark')
 let transparent = 0
 btn = document.querySelector('button')
 const overlay = document.querySelector('.overlay')
-const nextPrev = document.querySelectorAll('p')
-let select
-// можно использовать
+const next = document.querySelector('.next')
+const prev = document.querySelector('.prev')
+
 const pathImages = [
   'images/pic1.jpg',
   'images/pic2.jpg',
@@ -14,33 +14,52 @@ const pathImages = [
   'images/pic4.jpg',
   'images/pic5.jpg',
 ]
-function setSelect(e) {
-  select = document.querySelector('.select')
+// Функция которая добавляет класс
+function setSelect(obj) {
+  let select = document.querySelector('.select')
   if (select) {
     select.classList.remove('select')
-    e.target.classList.add('select')
+    obj.classList.add('select')
   } else {
-    e.target.classList.add('select')
+    obj.classList.add('select')
   }
-  displayedImage.setAttribute('src', e.target.getAttribute('src'))
+  displayedImage.setAttribute('src', obj.getAttribute('src'))
 }
+//Добавление слушателя объекты панели навигации(Боковые)
+next.addEventListener('click', () => {
+  const index = pathImages.indexOf(displayedImage.getAttribute('src'))
+  console.log('test right panel - ' + index)
+  if (index === pathImages.length - 1) {
+    setSelect(thumbBar.firstChild)
+  } else {
+    setSelect(thumbBar.children[index + 1])
+  }
+})
+prev.addEventListener('click', () => {
+  const index = pathImages.indexOf(displayedImage.getAttribute('src'))
+  console.log('test left panel - ' + index)
+  if (index === 0) {
+    setSelect(thumbBar.lastChild)
+  } else {
+    setSelect(thumbBar.children[index - 1])
+  }
+})
 
-for (let i = 0; i <= nextPrev.length - 1; i++) {
-  nextPrev[i].parentElement.addEventListener('click', (e) => {
-    console.log('test right and left panel' + e)
-  })
-}
-
+//Создание img и добавление его в бар-панель
 for (let i = 0; i <= pathImages.length - 1; i++) {
   var newImage = document.createElement('img')
   newImage.setAttribute('src', pathImages[i])
   thumbBar.appendChild(newImage)
 }
 
-thumbBar.addEventListener('click', setSelect)
+//Установка слушателя для установки активной страницы
+for (let i = 0; i <= thumbBar.children.length - 1; i++) {
+  thumbBar.children[i].addEventListener('click', (e) => {
+    setSelect(e.target)
+  })
+}
 
 dark.addEventListener('click', () => {
-  console.log('test click')
   transparent++
   if (transparent === 10) {
     overlay.style.backgroundColor = 'rgba(0,0,0,1)'
@@ -51,4 +70,3 @@ dark.addEventListener('click', () => {
     overlay.style.backgroundColor = 'rgba(0,0,0,0.' + transparent + ')'
   }
 })
-/* Wiring up the Darken/Lighten button */
